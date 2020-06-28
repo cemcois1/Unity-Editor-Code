93568bf1d2de7ea9c053d19f8d174fc922c0d0e4
cemal can yıldırım <56302638+cemcois1@users.noreply.github.com>
2020-06-28 19:09:10 +0300

    28.06.2020
    birden fazla objeye hit oluyor onu çözmem gerekiyor üçgen ise sıkıntılı

--- Editor/Assets/Prefabs/selectandmovecode.cs
+++ Editor/Assets/Prefabs/selectandmovecode.cs
@@ -1,36 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class selectandmovecode : MonoBehaviour
 {
-    Vector2 selectedPosition;
+    private Vector2 selectedPosition;
     private GameObject saklananObjem;
     public Color colorone;
-
 
     void Update()
     {
         selectedPosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
         Vector2 ray = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
-        RaycastHit2D hit;
-        hit = Physics2D.Raycast(Camera.main.transform.position, selectedPosition);
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.transform.position, selectedPosition);
+        
 
 
-
-        if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject()&& hit.collider.CompareTag("Prefab"))
+        if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject() && hit.collider.CompareTag("Prefab")  )
         {
 
             try
             {
-                //if (hit.collider.CompareTag("Prefab"))
-                {
-                    //print(hit.collider.name);
-                    hit.collider.gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
-                    hit.collider.gameObject.transform.position = selectedPosition;
-                    saklananObjem = hit.collider.gameObject;
-                }
+
+                hit.collider.gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+
+                hit.collider.gameObject.transform.position = selectedPosition;
+                hit.collider.gameObject.transform.position = new Vector3(hit.transform.position.x, hit.transform.position.y, -1);
+                saklananObjem = hit.collider.gameObject;
+
             }
             catch (System.NullReferenceException hata)
             {
@@ -38,11 +34,13 @@ public class selectandmovecode : MonoBehaviour
                 throw new System.NullReferenceException("Farkındayım", hata);
 
             }
+
         }
         else if (Input.GetMouseButtonUp(0) && !EventSystem.current.IsPointerOverGameObject() && hit.collider.CompareTag("Prefab"))
         {
 
             hit.collider.gameObject.GetComponent<MeshRenderer>().material.color = Color.white;
+            hit.collider.gameObject.transform.position = new Vector3(hit.transform.position.x, hit.transform.position.y, 0);
 
         }
 
@@ -57,10 +55,17 @@ public class selectandmovecode : MonoBehaviour
         saklananObjem.transform.localScale = saklananObjem.transform.localScale * (.5f);
 
     }
-    public void turnLeft() {
-        saklananObjem.transform.rotation =new Quaternion(saklananObjem.transform.rotation.x, saklananObjem.transform.rotation.y, saklananObjem.transform.rotation.z+45, saklananObjem.transform.rotation.w);
+    public void turnLeft()
+    {
+        saklananObjem.transform.Rotate(new Vector3(0, 0, 30));
     }
-    public void turnRight() { 
-        saklananObjem.transform.rotation =new Quaternion(saklananObjem.transform.rotation.x, saklananObjem.transform.rotation.y, saklananObjem.transform.rotation.z-45, saklananObjem.transform.rotation.w);
+    public void turnRight()
+    {
+        saklananObjem.transform.Rotate(new Vector3(0, 0, -30));
+
+    }
+    public void lenghtUp()
+    {
+        saklananObjem.transform.localScale = new Vector3(saklananObjem.transform.localScale.x+1, saklananObjem.transform.localScale.y, saklananObjem.transform.localScale.z);
     }
 }
