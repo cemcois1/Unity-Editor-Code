c19a07dd7ac9eaef58f13d1ca8c9239b11e5c21a
cemal can yıldırım <56302638+cemcois1@users.noreply.github.com>
2020-07-28 14:11:30 +0300

    28.07.2020
    prefab olarak kaydetme işlemi için butonun basılabilir olması gerekiyor bu buton şu an basılabilir değil nedenini bilmiyorum ama geri kalan çalışıyor
    artık objelerim rahat rahat hareket ettirilebiliyor

--- Editor/Assets/Prefabs/selectandmovecode.cs
+++ Editor/Assets/Prefabs/selectandmovecode.cs
@@ -1,21 +1,11 @@
 ﻿using UnityEngine;
+using UnityEditor;
 using System.Collections;
-public class saveddata
-{
-    public Vector2 pozisyon;
 
-    public saveddata(Vector2 vec)
-    {
-        pozisyon.x = vec.x;
-        pozisyon.y = vec.y;
-
-
-    }
-
-}
 public class selectandmovecode : MonoBehaviour
 {
     [SerializeField] GameObject nokta;
+    [SerializeField] GameObject kaydetbutton;
     Renderer sideobjrend;
     [SerializeField] GameObject Myscene;
     [SerializeField] GameObject[] prefabs;
@@ -23,17 +13,12 @@ public class selectandmovecode : MonoBehaviour
     public GameObject gameScene;
     public GameObject[] buttons;
     public GameObject text;
+    public GameObject parent;
+
     ArrayList arrayList = new ArrayList();
-    void Start()
-    {
-
-    }
-
-
 
     void Update()
     {
-
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit2D hit;
         hit = Physics2D.Raycast(ray.origin, ray.direction * 20);
@@ -44,25 +29,23 @@ public class selectandmovecode : MonoBehaviour
 
 
 
+
     }
     private void selectPoints(RaycastHit2D hit)
     {
         if (Input.GetMouseButtonDown(0) && Karaktersahnesimi)
         {
+            print(hit.collider.name);
+           // if (hit.collider.name)
+            {
+
+            }
             //noktalar adında bir  prefabım olsun  -------
             // prefabım  rayin hit ettiği noktada oluştursun--------
-            Instantiate(nokta, new Vector3(hit.point.x, hit.point.y, -1), Quaternion.identity);
+            Instantiate(nokta, new Vector3(hit.point.x, hit.point.y, -1), Quaternion.identity, parent.transform);
             //noktaları 3 lü olacak şekilde bir diziye kaydetsin ve çizsin 
             arrayList.Add(hit.point.x);
             arrayList.Add(hit.point.y);
-            for (int i = 0; i < arrayList.Count; i++)
-            {
-                print(arrayList[i]);
-
-            }
-
-
-
 
 
             //sonra da başka 3 lü noktalar seçilir bitti butonuna basılana kadar
@@ -78,15 +61,20 @@ public class selectandmovecode : MonoBehaviour
         {
             if (hit.transform.gameObject.CompareTag("Prefab"))
             {
+                hit.collider.GetComponent<BoxCollider2D>().size = new Vector2(5, 5);
                 sideobjrend = hit.collider.GetComponent<Renderer>();
+
                 hit.collider.gameObject.transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, -1);
                 sideobjrend.material.SetColor("_Color", Color.blue);
+
 
             }
 
         }
         else if (Input.GetMouseButtonUp(0) && !Karaktersahnesimi && hit.transform.gameObject.CompareTag("Prefab"))
         {
+            hit.collider.GetComponent<BoxCollider2D>().size = new Vector2(0.75f, 0.75f);
+
             sideobjrend.material.SetColor("_Color", Color.white);
             sideobjrend.transform.position.Set(sideobjrend.transform.position.x, sideobjrend.transform.position.y, 0);
 
@@ -124,18 +112,39 @@ public class selectandmovecode : MonoBehaviour
 
 
     }
+    public void kaydet()
+    {
+        print("girdi");
+        bool basarilimi = false;
+        //eğer kaydet butonuna basılırsa  karakter prefab olarak kaydet  noktaları
+        UnityEditor.PrefabUtility.SaveAsPrefabAsset(parent,assetPath: "Assets/Prefabs/Karakter", out basarilimi);
+        if (basarilimi == false)
+        {
+            print("basarisiz");
+
+        }
+        if (basarilimi == true)
+        {
+            print("basarili");
+
+        }
+
+
+    }
     public void craftCharacter()
     {
         Karaktersahnesimi = true;
         //ekrandaki  herşeyi kapat 
         gameScene.SetActive(false);
+
         //butonları da kapat
         foreach (GameObject butonlar in buttons)
         {
             butonlar.SetActive(false);
         }
         text.SetActive(true);
-
+        //kaydet buttonunu aç
+        kaydetbutton.SetActive(true);
 
     }
 }
